602eaa4a75794c79a514f9ebed7e2503581fd2c5
Jaime Gerst <jaimegerst1@gmail.com>
2020-08-24 12:11:45 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Text, View, StyleSheet, Picker, Switch, Button, ScrollView, Alert } from "react-native";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
   
@@ -30,6 +32,28 @@ class Reservation extends Component {
       date: "",
       showModal: false,
     });
+  }
+
+  async obtainNotificationPermission() {
+      const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+      if (permission.status !== 'granted') {
+          const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+          if (permission.status !== 'granted') {
+            Alert.alert('Permission not granted to show notifications');
+          }
+          return permission;
+      }
+      return permission;
+  }
+
+  async presentLocalNotification(date) {
+      const permission = await this.obtainNotificationPermission();
+      if (permission.status === 'granted') {
+          Notifications.presentLocalNotificationAsync({
+              title: 'Your Campsite Reservation Search',
+              body: 'Search for ' + date + ' requested'
+          });
+      }
   }
 
   render() {
@@ -102,11 +126,14 @@ class Reservation extends Component {
                                         onpress: () => this.resetForm()
                                     },
                                     {
-                                        text: 'OK',
-                                        onpress: () => this.resetForm()
+                                        text: 'OK', 
+                                        onPress: () => {
+                                            this.presentLocalNotification(this.state.date);
+                                            this.resetForm();
+                                        }
                                     }
                                 ],
-                                {cancelable: false}
+                                { cancelable: false }
                             );
                         }}
                         title='Search'
