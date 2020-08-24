e89c5f89747cfda29981d65fe8907ceb690045d9
Jaime Gerst <jaimegerst1@gmail.com>
2020-08-24 11:25:31 -0400

    Secure Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, ShadowPropTypesIOS } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -28,6 +28,8 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -55,7 +57,9 @@ function RenderCampsite(props) {
                     { cancelable: false }
                 );
             }
-            return true;
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal()
+            }
         }
     });
 
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,3 @@
 export const baseUrl = 'http://192.168.1.73:3001/';
+
+
